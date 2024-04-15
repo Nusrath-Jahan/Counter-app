9de6de0e8ad8ecbe9eb063e3927ce77387c76a6c
Nusrath <nusrathjahan0204@gmail.com>
2024-04-15 17:00:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let name = 'nusrat';
+console.log(name);
+
+let counterValue = 0;
+function updateCounterDisplay(){
+    document.getElementById('counter').textContent = counterValue ;
+}
+function increaseCounter(){
+    counterValue++ ;
+    updateCounterDisplay();
+}
+function decreaseCounter(){
+    counterValue-- ;
+    updateCounterDisplay() ;
+}
+function resetCounter(){
+    counterValue = 0 ;
+    updateCounterDisplay();
+}
